c6f0145fe3f583861d9e2a86eb48f0dd2e728b73
Miguelmdcc <108490537+Miguelmdcc@users.noreply.github.com>
2024-04-15 22:22:49 -0300

    Fixed functions

--- main.py
+++ main.py
@@ -5,63 +5,63 @@ x = np.array([[1,0.5,-1],
               [0,0.5,1],
               [1,-0.5,-1]])
 
+#definindo targets em one of classes
+targ = np.array([[1,-1,-1],
+                [-1,1,-1],
+                [-1,-1,1]])
+
 #Alfa(taxa de aprendizagem)
 alfa  = 0.01
 
 #Quantidade de neuronios na camada intermediaria
 neuronios = 2
 
-#Primeira camada
-#Pesos sinapticos aleatorios entre -0,5 e 0,5
-# v = np.random.uniform(-0.5, 0.5, size=(x.shape[0],neuronios))
-# print(v)
-#Pesos do exemplo dos slides
-v = np.array([[0.12, -0.03],
-              [-0.04, 0.15],
-              [0.31, -0.41]])
-
-#Bias aleatorios entre -0.5 e 0.5
-# v0 = np.random.uniform(-0.5, 0.5, size=neuronios)
-# print(v0)
-v0 = np.array([-0.09,0.18])
-
-#Segunda camada
-#Pesos sinapticos aleatorios entre -0,5 e 0,5
-# w = np.random.uniform(-0.5, 0.5, size=(neuronios,targ.shape[0]))
-# print(w)
-#Pesos do exemplo dos slides
-w = np.array([[-0.05, -0.34,0.21],
-              [0.19,-0.09,0.26]])
-
-#Bias aleatorios entre -0.5 e 0.5
-# w0 = np.random.uniform(-0.5, 0.5, size=targ.shape[0])
-# print(w0)
-w0 = np.array([0.18,-0.27,-0.12])
-
 #Vamos usar a função de ativação tangente hiperbolica
 # np.tanh()
 
-#definindo targets em one of classes
-targ = np.array([[1,-1,-1],
-                [-1,1,-1],
-                [-1,-1,1]])
-
-zin = 0
-y = 0
-z = 0
-errototal = 1
-quadratico = 0
-deltak = 0
-deltaw = 0
-deltaw0 = 0
-deltain = 0
-delta = 0
-deltav = 0
-deltav0 = 0
-ciclo = 0
-errotolerado = 0.01
-
 def treino():
+    zin = 0
+    y = 0
+    z = 0
+    errototal = 1
+    quadratico = 0
+    deltak = 0
+    deltaw = 0
+    deltaw0 = 0
+    deltain = 0
+    delta = 0
+    deltav = 0
+    deltav0 = 0
+    ciclo = 0
+    errotolerado = 0.01
+
+    #Primeira camada
+    #Pesos sinapticos aleatorios entre -0,5 e 0,5
+    # v = np.random.uniform(-0.5, 0.5, size=(x.shape[0],neuronios))
+    # print(v)
+    #Pesos do exemplo dos slides
+    v = np.array([[0.12, -0.03],
+                [-0.04, 0.15],
+                [0.31, -0.41]])
+
+    #Bias aleatorios entre -0.5 e 0.5
+    # v0 = np.random.uniform(-0.5, 0.5, size=neuronios)
+    # print(v0)
+    v0 = np.array([-0.09,0.18])
+
+    #Segunda camada
+    #Pesos sinapticos aleatorios entre -0,5 e 0,5
+    # w = np.random.uniform(-0.5, 0.5, size=(neuronios,targ.shape[0]))
+    # print(w)
+    #Pesos do exemplo dos slides
+    w = np.array([[-0.05, -0.34,0.21],
+                [0.19,-0.09,0.26]])
+
+    #Bias aleatorios entre -0.5 e 0.5
+    # w0 = np.random.uniform(-0.5, 0.5, size=targ.shape[0])
+    # print(w0)
+    w0 = np.array([0.18,-0.27,-0.12])
+    
     while(errototal > errotolerado):
         errototal = 0
         for i in range(x.shape[0]):
@@ -100,19 +100,21 @@ def treino():
 
         ciclo += 1
         print("Ciclo:", ciclo, "Erro total:", errototal)
+    
+    return[v,v0,w,w0]
 
 # Novos padrões de entrada para teste
 novos_padroes = x
 
-def treino(entrada,v,v0,w,w0):
+def teste(entrada,peso1,bias1,peso2,bias2):
     # Realizar a propagação direta para obter as previsões
     for padrao in entrada:
         # Cálculo da primeira camada
-        zin = np.dot(padrao, v) + v0
+        zin = np.dot(padrao, peso1) + bias1
         z = np.tanh(zin)
 
         # Cálculo da segunda camada
-        yin = np.dot(z, w) + w0
+        yin = np.dot(z, peso2) + bias2
         y = np.tanh(yin)
         
         for j in range(targ.shape[0]):
@@ -123,4 +125,6 @@ def treino(entrada,v,v0,w,w0):
 
         print("Padrão de entrada:", padrao)
         print("Saída prevista:", y)
-treino(novos_padroes,v,v0,w,w0)
+
+pesos = treino()
+teste(novos_padroes,pesos[0],pesos[1],pesos[2],pesos[3])
